5fdc2e33504a5caf74483b35e5acc1190691c792
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-03-05 18:32:46 -0500

    feat: show sample data

---
+++ react-ecommerce/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products:[
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p2.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'sample product',
+        },
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'sample product',
+        },
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p2.png',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'sample product',
+        },
+    ],
+};
+
+export default data;
