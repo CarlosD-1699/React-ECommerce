431386a8e5820977f4a36c2d255d8406077510b0
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-03-10 21:20:20 -0500

    feat: Add react context, storeprovider and usethem

--- react-ecommerce/src/Views/ProductView/ProductView.jsx
+++ react-ecommerce/src/Views/ProductView/ProductView.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -13,6 +13,7 @@ import { Helmet } from "react-helmet-async";
 import { LoadingBox } from "../../Components/Loading/LoadingBox";
 import MessageBox from "../../Components/MessageBox/MessageBox";
 import { getError } from "../../Utils/utils";
+import { Store } from "../../Utils/store";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -49,6 +50,14 @@ const ProductView = () => {
     };
     fetchData();
   }, [slug]);
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const addToCartHandler = () => {
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...product, quantity: 1 },
+    });
+  };
 
   return loading ? (
     <LoadingBox />
@@ -103,7 +112,9 @@ const ProductView = () => {
                 {product.countInStock > 0 && (
                   <ListGroup.Item>
                     <div className="d-grid">
-                      <Button variant="primary">Add to Cart</Button>
+                      <Button onClick={addToCartHandler} variant="primary">
+                        Add to Cart
+                      </Button>
                     </div>
                   </ListGroup.Item>
                 )}
