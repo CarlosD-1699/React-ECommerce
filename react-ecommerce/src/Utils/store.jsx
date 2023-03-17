723bf61d0ab0276c50aa85ecfd26bca646ff2497
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-03-16 19:48:29 -0500

    feat: function to save address data in loclstorage

--- react-ecommerce/src/Utils/store.jsx
+++ react-ecommerce/src/Utils/store.jsx
@@ -4,10 +4,14 @@ export const Store = createContext();
 
 const initialState = {
   userInfo: localStorage.getItem("userInfo")
-      ? JSON.parse(localStorage.getItem("userInfo"))
-      : null,
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
 
   cart: {
+    shippingAddress: localStorage.getItem("shippingAddress")
+      ? JSON.parse(localStorage.getItem("shippingAddress"))
+      : {},
+
     cartItems: localStorage.getItem("cartItems")
       ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
@@ -39,6 +43,8 @@ function reducer(state, action) {
       return { ...state, userInfo: action.payload };
     case "USER_SIGNOUT":
       return { ...state, userInfo: null };
+    case "SAVE_SHIPPING_ADDRESS":
+      return { ...state, cart: { ...state.cart, shippingAddress: action.payload, } };
     default:
       return state;
   }
--- react-ecommerce/src/Views/ShippingAddressView/ShippingAddressView.jsx
+++ react-ecommerce/src/Views/ShippingAddressView/ShippingAddressView.jsx
@@ -1,19 +1,48 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Button from "react-bootstrap/esm/Button";
 import Container from "react-bootstrap/esm/Container";
 import { Helmet } from "react-helmet-async";
-import Form from 'react-bootstrap/Form';
-import "./ShippingAddress.css"
+import Form from "react-bootstrap/Form";
+import "./ShippingAddress.css";
+import { Store } from "../../Utils/store";
+import { useNavigate } from "react-router-dom";
 
 const ShippingAddressView = () => {
-  const [fullName, setFullName] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [country, setCountry] = useState("");
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress },
+  } = state;
+
+  const [fullName, setFullName] = useState(shippingAddress.fullName || '');
+  const [address, setAddress] = useState(shippingAddress.address || '');
+  const [city, setCity] = useState(shippingAddress.city || '');
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || '');
+  const [country, setCountry] = useState(shippingAddress.country || '');
 
   const submitHandler = (e) => {
     e.preventDefault();
+    ctxDispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: {
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      },
+    });
+    localStorage.setItem(
+      "shippingAddress",
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    );
+    navigate("/payment");
   };
 
   return (
@@ -21,52 +50,56 @@ const ShippingAddressView = () => {
       <Helmet>
         <title>Shipping Address</title>
       </Helmet>
-      <h1 className="my-3">Shipping Address</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="fullName">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="postalCode">
-          <Form.Label>Postal code</Form.Label>
-          <Form.Control
-            value={postalCode}
-            onChange={(e) => setPostalCode(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="country">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-            required
-          ></Form.Control>
-        </Form.Group>
-        <div className="mb-3">
-          <Button variant="primary" type="submit">Continue</Button>
-        </div>
-      </Form>
+      <div className="container small-container">
+        <h1 className="my-3">Shipping Address</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="fullName">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="address">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="postalCode">
+            <Form.Label>Postal code</Form.Label>
+            <Form.Control
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+            ></Form.Control>
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary" type="submit">
+              Continue
+            </Button>
+          </div>
+        </Form>
+      </div>
     </Container>
   );
 };
