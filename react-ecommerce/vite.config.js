5b6c12175fee3ce5000364a5442cdc3b17182a01
Carlos Barbosa <125832186+CarlosD-1699@users.noreply.github.com>
2023-03-28 23:53:09 -0500

    feat: proxy

--- react-ecommerce/vite.config.js
+++ react-ecommerce/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
+      // string shorthand
+      "/foo": "http://localhost:5000",
       "/api": {
         target: "https://back-ecommerce-wu5w.onrender.com/",
+        changeOrigin: true,
         secure: false,
       },
     },
   },
   plugins: [react()],
-  base: '/React-ECommerce/'
-})
+  base: "/React-ECommerce/",
+});
